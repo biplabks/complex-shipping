3c507ec2fdbe49e90d782d3c1b178397c6435958
Biplab Saha <bjbs.anjoy@gmail.com>
2022-09-30 09:18:31 -0500

    Added updated code

--- src/pages/TableTest.js
+++ src/pages/TableTest.js
@@ -23,7 +23,6 @@ class TableTest extends React.Component {
           dataField: 'item',
           text: 'Item',
           validator: this.validatorFormatter,
-          editable: this.editableTester,
           style: {
             width: '1000px',
             textAlign: 'center'
@@ -47,11 +46,38 @@ class TableTest extends React.Component {
             }
             return true;
           },
-          editable: this.editableTester,
-          // formatter: this.headerStyle
           style: {
             width: '140px',
             textAlign: 'center'
+          }
+        },
+        {
+          dataField: "remove",
+          text: "Delete",
+          /*
+          formatter: (cellContent, row) => {
+            return (
+              <button
+                className="btn btn-danger btn-xs"
+                onClick={() => this.handleDelete(row)}
+              >
+                Delete
+              </button>
+            );
+          }
+          */
+          formatter: (cellContent, row) => {
+            return (
+              <button
+                className="btn btn-danger btn-xs"
+                onClick={() => this.handleDelete(row)}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                  <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+              </button>
+            );
           }
         }
       ],
@@ -59,11 +85,34 @@ class TableTest extends React.Component {
     };
   }
 
-  editableTester = () => {
-    // return !this.context.isSubmitButtonLoading
-    return true 
-  }
+  handleDelete = (row) => {
+    var existingItem = this.state.items;
 
+    var modifiedData = [];
+    existingItem.forEach(element => {
+        if (element.id != row.id) {
+          modifiedData.push(element);
+        }
+    })
+
+    for (let index = 0; index < modifiedData.length; index++) {
+      modifiedData[index]['id'] = index
+    }
+
+    var existingItemByDueDate = this.context['itemsByDueDate']
+    for (let index = 0; index < existingItemByDueDate.length; index++) {
+      if(existingItemByDueDate[index]['key'] == this.state.due_date) {
+        existingItemByDueDate[index]['value'] = Object.assign([], modifiedData);
+        break
+      }
+    }
+
+    this.context['itemsByDueDateMap'].set(this.state.due_date, modifiedData)
+
+    this.setState({items: modifiedData}, () => {
+      console.log("modified items: ", this.state.items);
+    })
+  };
   validatorFormatter = (newValue, row, column) => {
     var exists = false;
     for (let index = 0; index < this.state.items.length; index++) {
@@ -108,7 +157,6 @@ class TableTest extends React.Component {
 
   handleBtnClick = () => {
     let value = this.context;
-    value['cars']['car001']['name'] = 'Biplab1'
 
     var existingItem = this.state.items;
     var selectedItems = this.state.selected;
@@ -137,59 +185,6 @@ class TableTest extends React.Component {
     value['itemsByDueDateMap'].set(this.state.due_date, modifiedData)
   }
 
-  handleOnSelect = (row, isSelect) => {
-    if (isSelect) {
-      this.setState(() => ({
-        selected: [...this.state.selected, row.id]
-      }));
-    } else {
-      this.setState(() => ({
-        selected: this.state.selected.filter(x => x !== row.id)
-      }));
-    }
-  }
-
-  handleOnSelectAll = (isSelect, rows) => {
-    const ids = rows.map(r => r.id);
-    if (isSelect) {
-      this.setState(() => ({
-        selected: ids
-      }));
-    } else {
-      this.setState(() => ({
-        selected: []
-      }));
-    }
-  }
-
-  removeItemByDueDate = () => {
-    var existingItem = this.state.items;
-    var selectedItems = this.state.selected;
-
-    var modifiedData = [];
-    existingItem.forEach(element => {
-        if (!selectedItems.includes(element.id)) {
-          modifiedData.push(element);
-        }
-    })
-
-    for (let index = 0; index < modifiedData.length; index++) {
-      modifiedData[index]['id'] = index
-    }
-
-    this.setState({items: modifiedData, selected: []})
-
-    var existingItemByDueDate = this.context['itemsByDueDate']
-    for (let index = 0; index < existingItemByDueDate.length; index++) {
-      if(existingItemByDueDate[index]['key'] == this.state.due_date) {
-          existingItemByDueDate[index]['value'] = Object.assign([], modifiedData);
-          break
-      }
-    }
-
-    this.context['itemsByDueDateMap'].set(this.state.due_date, modifiedData)
-  }
-
   insertItemByDueDate = () => {
     this.context.insertItemByDueDate(this.state.due_date);
     this.setState({
@@ -207,108 +202,45 @@ class TableTest extends React.Component {
   }
 
   render() {
-    const selectRow = {
-      mode: 'checkbox',
-      clickToSelect: true,
-      clickToEdit: true,
-      selected: this.state.selected,
-      onSelect: this.handleOnSelect,
-      onSelectAll: this.handleOnSelectAll,
-      // style: {
-      //   width: '100px'
-      // }
-    };
-      return (
-        <Container className="p-3">
-          {
-            <div>
-              <Form>
-                <Form.Group as={Row} className="mb-3" controlId="formGroupDueDate">
-                  <Form.Label style={{display: 'flex', justifyContent:'left'}} column sm="1">
-                    Due Date
-                  </Form.Label>
-                  <Col sm="2" className='m-6'>
-                    <Form.Control onChange={event => this.setDueDate(event)} disabled={!this.state.isDateEditable} type="date" value={moment(this.state.due_date).utc().format('YYYY-MM-DD')} placeholder="Enter date" />
-                  </Col>
-                  {!this.context.isConfirmed && 
-                    <>
-                      {/* <Col className='float-left'>
-                        <Button disabled={this.context.isSubmitButtonLoading} onClick={ this.removeItemByDueDate }>
-                          Remove records
-                        </Button>
-                      </Col>
-                      <Col  className='float-left'>
-                        <Button disabled={this.context.isSubmitButtonLoading} onClick={this.insertItemByDueDate}>
+    return (
+      <Container className="p-3">
+        {
+          <div>
+            <Form>
+              <Form.Group as={Row} className="mb-3" controlId="formGroupDueDate">
+                <Form.Label style={{display: 'flex', justifyContent:'left'}} column sm="1">
+                  Due Date
+                </Form.Label>
+                <Col sm="2" className='m-6'>
+                  <Form.Control onChange={event => this.setDueDate(event)} disabled={!this.state.isDateEditable} type="date" value={moment(this.state.due_date).utc().format('YYYY-MM-DD')} placeholder="Enter date" />
+                </Col>
+                {!this.context.isConfirmed && 
+                  <>
+                    <Col className='d-flex flex-row mb-3'>
+                        <Button className='mx-1' disabled={this.context.isSubmitButtonLoading} onClick={this.insertItemByDueDate}>
                           Insert Record
                         </Button>
-                      </Col>
-                      <Col sm="3" className='m-6'>
-                        <Button disabled={this.context.isSubmitButtonLoading} onClick={this.copyItemsByDueDate}>
+                        <Button className='mx-1' disabled={this.context.isSubmitButtonLoading} onClick={this.copyItemsByDueDate}>
                           Copy This Table
                         </Button>
-                      </Col> */}
-                      <Col className='d-flex flex-row mb-3'>
-                        {/* <ButtonGroup aria-label="Basic example"> */}
-                          <Button className='mx-1' disabled={this.context.isSubmitButtonLoading} onClick={ this.removeItemByDueDate }>
-                            Remove records
-                          </Button>
-                          <Button className='mx-1' disabled={this.context.isSubmitButtonLoading} onClick={this.insertItemByDueDate}>
-                            Insert Record
-                          </Button>
-                          <Button className='mx-1' disabled={this.context.isSubmitButtonLoading} onClick={this.copyItemsByDueDate}>
-                            Copy This Table
-                          </Button>
-                        {/* </ButtonGroup> */}
-                      </Col>
-                      
-                    </>
-                  }
-                </Form.Group>
-              </Form>
-            </div>
-          }
-          
-          {/* {
-            !this.context.isConfirmed &&
-            <div className='d-flex justify-content-between me-3'>
-              <form>
-                <Button disabled={this.context.isSubmitButtonLoading} onClick={ this.removeItemByDueDate }>
-                  Remove records
-                </Button>
-              </form>
-              <form>
-                <Button disabled={this.context.isSubmitButtonLoading} onClick={this.insertItemByDueDate}>
-                  Insert Record
-                </Button>
-              </form>
-              <form>
-                <Button disabled={this.context.isSubmitButtonLoading} onClick={this.copyItemsByDueDate}>
-                  Copy This Table
-                </Button>
-              </form>
-            </div>
-          } */}
-          {/* {
-            <div>
-              <br />
-            </div>
-          } */}
-          {/* <div style={{padding: "50px"}}> */}
-            <BootstrapTable
-              keyField="id"
-              // data={ items }
-              // data={ this.props.orderDetails }
-              data={ this.state.items }
-              columns={ this.state.columns }
-              cellEdit={ cellEditFactory({ mode: 'dbclick' }) }
-              noDataIndication="Table is Empty"
-              selectRow={selectRow}
-            />
-          {/* </div> */}
-          
-        </Container>
-      );
-    }
+                    </Col>
+                  </>
+                }
+              </Form.Group>
+            </Form>
+          </div>
+        }
+        <BootstrapTable
+          keyField="id"
+          data={ this.state.items }
+          columns={ this.state.columns }
+          cellEdit={ cellEditFactory({ mode: 'dbclick' }) }
+          noDataIndication="Table is Empty"
+        />
+        
+      </Container>
+    );
+  }
 };
 
 TableTest.contextType = MyContext;
